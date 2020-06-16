71bb4e5b6413200d7f6f0718a24e36db5c3b19ec
Simon Hall <simon970@gmail.com>
2020-06-16 17:19:15 +1200

    hotload css and content in browser with webpack

---
+++ client/components/Control.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Control extends React.Component {
+    state ={
+        checked: false,
+        range: 0
+    }
+
+    render() {
+        return (
+            <div className = 'controller'>
+
+            </div>
+        )
+    }
+}
+
+export default Control
