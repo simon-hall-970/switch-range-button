248fe1498608080da6f6269e4c194c0ea10b16af
Simon Hall <simon970@gmail.com>
2020-06-16 17:25:17 +1200

    add conditional class name to control div element

--- client/components/Control.jsx
+++ client/components/Control.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 class Control extends React.Component {
     state ={
-        checked: false,
+        checked: true,
         range: 0
     }
 
     render() {
         return (
-            <div className = 'controller'>
+            <div className = {this.state.checked ? 'controller checked' : 'controller'}
+
+            >
 
             </div>
         )
