0b2800ef6402cd8a13a1815479c368c10becda8a
Simon Hall <simon970@gmail.com>
2020-06-16 22:22:31 +1200

    tweaking functions for setting velocity

--- client/components/Control.jsx
+++ client/components/Control.jsx
@@ -4,7 +4,7 @@ class Control extends React.Component {
     state ={
         checked: true,
         rightBtnDown: false,
-        velocity: 0
+        velocity: 100
     }
     
     componentDidMount() {
@@ -22,35 +22,48 @@ class Control extends React.Component {
                 checked: !checkedState
             })
         }
-
         if (e.button === 2) {
             this.setState({
                 rightBtnDown: true
-            })
+            }, ()=>console.log(this.state.rightBtnDown))
         }
     }
 
     velocity = (evt) => {
-        console.log(evt.nativeEvent)
+        const e = evt.nativeEvent
+        let start = this.state.velocity
+        let movement = e.movementY
+        let velChange = () => {
+            if (start - movement < 0) {return 0}
+            else if (start - movement > 100) {return 100}
+            else {return start - movement}
+        }
+        let vel = velChange()
+        console.log('| movement: ', e.movementY, '| velocity: ', vel)
+        this.setState({
+            velocity: vel           
+        })
     }
 
     mouseUp = (evt) => {
+        let velocity = this.state.velocity
         if (evt.button === 2) {
             this.setState({
-                rightBtnDown: false
+                rightBtnDown: false,
             })
         }
+        console.log(this.state.velocity)
     }
 
     render() {
-        console.log(this.state.checked)
+
         return (
+            
             <div 
                 className = {this.state.checked ? 'controller checked' : 'controller'}
                 onMouseDown = {this.mouseDown}
                 onMouseMove = {this.state.rightBtnDown ? this.velocity : (e)=>null}
                 onContextMenu = {e => e.preventDefault()}
-
             >
 
             </div>
